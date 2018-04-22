8fe0ce5b8e8abd893d8e4295b48d9dbf3f61b29c
fkim2 <35535550+fkim2@users.noreply.github.com>
2018-04-22 14:05:20 -0400

    Update survey.php

--- survey.php
+++ survey.php
@@ -1,8 +1,13 @@
 <?php include "inc/html-top.inc"; ?>
 
+    <!--maybe we could have drop down options or radio buttons for options: rarely, sometimes, frequently -->
+
 	<h2>How often do you see trash on the ground?</h2>
 
 	<h2>How often do you pick up trash off the ground?</h2>
+
+    <!--this could be a text input -->
+    <h2>In what ways could YOU prevent further pollution?</h2>
 
  </body>
  </html>
