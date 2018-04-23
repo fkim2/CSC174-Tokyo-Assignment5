7bbbcee275bd5d1355f162ae6c3437b35a8c9129
fkim2 <35535550+fkim2@users.noreply.github.com>
2018-04-22 20:05:46 -0400

    Update impact.php

--- impact.php
+++ impact.php
@@ -1,7 +1,7 @@
 <?php include "inc/html-top.inc"; ?>
 	
 	<h2>Global Impact</h2>
-    <img src = "oceanPollution.jpg" alt = "trash floating in ocean">
+    <img src = "images/oceanPollution.jpg" alt = "trash floating in ocean">
 		
 	<h3>How Much Trash Enters the Ocean?</h3>
 	<p>Approximately 1.4 billion pounds of trash per year enters the ocean. Over 80% of marine pollution comes from land-based activities.
@@ -10,7 +10,7 @@
 	<h3>Impact on Marine Life</h3>
 	<p>Plastic garbage, which decomposes very slowly, is often mistaken for food by marine animals. High concentrations of plastic material, particularly plastic bags, have been found blocking the breathing passages and stomachs of many marine species, including whales, dolphins, seals, puffins, and turtles. Plastic six-pack rings for drink bottles can also choke marine animals.
 	</p>
-    <img src = "beachedWhale.jpg" alt = "beached, dead whale">
+    <img src = "images/beachedWhale.jpg" alt = "beached, dead whale">
 
 	<h3>Impact on Seafood</h3>
 	<p>Pollution can have an adverse affect on the food that we eat. Heavy metals and other contaminants can accumulate in seafood and make it harmful to eat. More than one-third of the shellfish-growing waters of the United States are adversely affected by coastal pollution.</p>
