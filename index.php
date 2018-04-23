f2c5cade38460c6c3c0c174c94fc4ce001d483a6
fkim2 <35535550+fkim2@users.noreply.github.com>
2018-04-22 20:06:05 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
  
 	<h2>The Problem: Marine Pollution</h2>
 	<p>The ocean covers almost three quarters of our planet. Populations in coastal regions are growing and placing increasing pressure on coastal and marine ecosystems. Marine pollution of many kinds threatens the health of the ocean and its living resources. While the past decades have seen efforts at the local, national, and international levels to address the problems of marine pollution, more needs to be done.</p>
-    <img src = "trashBeach.jpg" alt = "trash on beach">
+    <img src = "images/trashBeach.jpg" alt = "trash on beach">
 
 	<h2>The Importance of Our Oceans</h2>
 	<p>Covering more than 70 percent of our planet, oceans are among the earth’s most valuable natural resources. They govern the weather, clean the air, help feed the world, and provide a living for millions. They also are home to most of the life on earth, from microscopic algae to the blue whale, the largest animal on the planet. Yet we’re bombarding them with pollution. By their very nature—with all streams flowing to rivers, all rivers leading to the sea—the oceans are the end point for so much of the pollution we produce on land, however far from the coasts we may be. And from dangerous carbon emissions to choking plastic to leaking oil to constant noise, the types of ocean pollution humans generate are vast. As a result, collectively, our impact on the seas is degrading their health at an alarming rate.</p>
-    <img src = "beach.jpg" alt = "trash washed up on beach">
+    <img src = "images/beach.jpg" alt = "trash washed up on beach">
 
 </body>
 </html>
