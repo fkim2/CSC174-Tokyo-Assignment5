d682600f2d6340fe17f60109253e32d32bd27801
Conley <cernst3@u.rochester.edu>
2018-04-28 08:32:02 -0400

    first steps for layout

--- index.php
+++ index.php
@@ -1,4 +1,51 @@
-<?php include "inc/html-top.inc"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php include "inc/html-top.inc"; ?>
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	
+	<title></title>
+
+</head>
+<body>
+
+	<div class="w3-container">
+  <h3>Mobile First Fluid Grid</h3>
+  <p class="w3-large">Try to resize the window!</p>
+</div>
+
+<!-- <div class="w3-row">
+  <div class="w3-col m1 w3-center w3-grey">1</div>
+  <div class="w3-col m1 w3-center">2</div>
+  <div class="w3-col m1 w3-center w3-grey">3</div>
+  <div class="w3-col m1 w3-center">4</div>
+  <div class="w3-col m1 w3-center w3-grey">5</div>
+  <div class="w3-col m1 w3-center">6</div>
+  <div class="w3-col m1 w3-center w3-grey">7</div>
+  <div class="w3-col m1 w3-center">8</div>
+  <div class="w3-col m1 w3-center w3-grey">9</div>
+  <div class="w3-col m1 w3-center">10</div>
+  <div class="w3-col m1 w3-center w3-grey">11</div>
+  <div class="w3-col m1 w3-center">12</div>
+</div> -->
+
+<div class="w3-row">
+  <div class="w3-col w3-container m8 l9">  
+    <p>This part will occupy 12 columns on a small screen, 8 on a medium screen, and 9 on a large screen.</p>
+    <p>This part will occupy 12 columns on a small screen, 8 on a medium screen, and 9 on a large screen.</p>
+  </div>
+  <div class="w3-col w3-container m4 l3 w3-yellow">
+    <p>This part will occupy 12 columns on a small screen, 4 on a medium screen, and 3 on a large screen.</p>
+  </div>
+</div>
+
+</body>
+
+</body>
+</html>
+
+<!-- <body>
  
 	<h2>The Problem: Marine Pollution</h2>
 	<p>The ocean covers almost three quarters of our planet. Populations in coastal regions are growing and placing increasing pressure on coastal and marine ecosystems. Marine pollution of many kinds threatens the health of the ocean and its living resources. While the past decades have seen efforts at the local, national, and international levels to address the problems of marine pollution, more needs to be done.</p>
@@ -8,7 +55,52 @@
 	<p>Covering more than 70 percent of our planet, oceans are among the earth’s most valuable natural resources. They govern the weather, clean the air, help feed the world, and provide a living for millions. They also are home to most of the life on earth, from microscopic algae to the blue whale, the largest animal on the planet. Yet we’re bombarding them with pollution. By their very nature—with all streams flowing to rivers, all rivers leading to the sea—the oceans are the end point for so much of the pollution we produce on land, however far from the coasts we may be. And from dangerous carbon emissions to choking plastic to leaking oil to constant noise, the types of ocean pollution humans generate are vast. As a result, collectively, our impact on the seas is degrading their health at an alarming rate.</p>
     <img src = "images/beach.jpg" alt = "trash washed up on beach">
 
+</body> -->
+
+
+
+
+
+
+<!-- 
+<body>
+
+<div class="w3-container">
+  <h3>Mobile First Fluid Grid</h3>
+  <p class="w3-large">Try to resize the window!</p>
+</div>
+
+<div class="w3-row">
+  <div class="w3-col m1 w3-center w3-grey">1</div>
+  <div class="w3-col m1 w3-center">2</div>
+  <div class="w3-col m1 w3-center w3-grey">3</div>
+  <div class="w3-col m1 w3-center">4</div>
+  <div class="w3-col m1 w3-center w3-grey">5</div>
+  <div class="w3-col m1 w3-center">6</div>
+  <div class="w3-col m1 w3-center w3-grey">7</div>
+  <div class="w3-col m1 w3-center">8</div>
+  <div class="w3-col m1 w3-center w3-grey">9</div>
+  <div class="w3-col m1 w3-center">10</div>
+  <div class="w3-col m1 w3-center w3-grey">11</div>
+  <div class="w3-col m1 w3-center">12</div>
+</div>
+
+<div class="w3-row">
+  <div class="w3-col w3-container m4 l3 w3-yellow">
+    <p>This part will occupy 12 columns on a small screen, 4 on a medium screen, and 3 on a large screen.</p>
+  </div>
+  <div class="w3-col w3-container m8 l9">  
+    <p>This part will occupy 12 columns on a small screen, 8 on a medium screen, and 9 on a large screen.</p>
+    <p>This part will occupy 12 columns on a small screen, 8 on a medium screen, and 9 on a large screen.</p>
+  </div>
+</div>
+
 </body>
-</html>
+
+<aside>
+	<p> Side bar content</p>
+</aside>
+
+</html> -->
 
 
