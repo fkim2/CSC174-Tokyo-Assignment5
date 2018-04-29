661a5cd359b8d0646bc5d29dafa8274d5f07a284
Isaiah Pule <ipule@u.rochester.edu>
2018-04-29 03:34:28 -0400

    I have added the login form, survery form, welcome page, and registration form, still working on database and all forms together.

---
+++ surv-signup.php
@@ -0,0 +1,60 @@
+<?php include "inc/html-top.inc"; ?>
+
+	<h1>Thank You for Subscribing</h1>
+<body>
+
+	<h1>Thank You for Subscribing</h1>
+<!--php code for sql connection and modifications-->
+          <?php
+	          $dbhost = "_____";
+	          $dbuser = "____";
+	          $dbpass = "____";
+	          $dbname = "____";
+	          $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+	          $SeeTrash = trim(stripcslashes($_POST['SeeTrash']));
+	          $PickTrash = trim(stripcslashes($_POST['PickTrash']));
+	          $PrevTrash = trim(stripcslashes($_POST['PrevTrash']));
+
+	          $query = "INSERT INTO ____ (SeeTrash, PickTrash, PrevTrash) VALUES ('$SeeTrash','$PickTrash','$PrevTrash')";
+	          $result = mysqli_query($connection, $query);
+
+          ?>
+
+            <h3>You've submitted:</h3>
+              <p>
+
+              How often do you see trash on the ground?: <?php echo $SeeTrash; ?><br>
+              How often do you pick up trash off the ground?: <?php echo $PickTrash; ?><br>
+              In what ways could YOU prevent further pollution?: <?php echo $PrevTrash; ?><br>
+				
+				<p>To go back, click:
+				<a href = "index.php">Here</a></p>
+					
+              </p>
+
+              <?php
+
+					if ($result) {
+
+						echo "Thank you we've received your information!";
+
+					} else {
+
+						die ("Database query failed!");}
+
+ 				?>
+          <?php
+          //close databse connection
+          mysqli_close($connection);
+          ?>
+
+        </div>
+
+<!--Ending tags-->
+
+<?php include "inc/footer.inc";?>
+
+</body>
+
+</html>
