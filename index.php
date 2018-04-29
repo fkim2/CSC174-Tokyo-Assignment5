1290c46ee0adc8134a41ef8632fdbb420609ff29
Conley <cernst3@u.rochester.edu>
2018-04-29 12:14:28 -0400

    few changes

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php include "inc/html-top.inc"; ?>
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<?php include "inc/css.inc"; ?>
 
 	
 	<title></title>
@@ -13,10 +13,8 @@
 <body>
 
 <div class="content">
-	<div class="w3-container">
-  		<h3>Mobile First Fluid Grid</h3>
-  		<!-- <p class="w3-large">Try to resize the window!</p> -->
-	</div>
+<div class="w3-container">
+  		<!-- <h3>Mobile First Fluid Grid</h3> -->
 
 <!-- <div class="w3-row">
   <div class="w3-col m1 w3-center w3-grey">1</div>
@@ -34,25 +32,29 @@
 </div> -->
 
 	<div class="w3-row">
-		<div class="w3-col w3-container m8 l9"> 
-			<h2>The Problem: Marine Pollution</h2> 
-			<p>The ocean covers almost three quarters of our planet. Populations in coastal regions are growing and placing increasing pressure on coastal and marine ecosystems. Marine pollution of many kinds threatens the health of the ocean and its living resources. While the past decades have seen efforts at the local, national, and international levels to address the problems of marine pollution, more needs to be done.</p>
+		<h2>The Problem: Marine Pollution</h2>
+		<div class="w3-col w3-container m8 l9">
 			<img src = "images/trashBeach.jpg" alt = "trash on beach">
 		</div>
-		<div class="w3-col w3-container m4 l3 w3-yellow">
-			<p>This part will occupy 12 columns on a small screen, 4 on a medium screen, and 3 on a large screen.</p>
+		<div class="w3-col w3-container m4 l3">
+			<p>The ocean covers almost three quarters of our planet. Populations in coastal regions are growing and placing increasing pressure on coastal and marine ecosystems. Marine pollution of many kinds threatens the health of the ocean and its living resources. While the past decades have seen efforts at the local, national, and international levels to address the problems of marine pollution, more needs to be done.</p>		
 		</div>
+		<!-- <h2>The Problem: Marine Pollution</h2>
+		<div class="w3-col w3-container w3-half "> 
+			<img src = "images/trashBeach.jpg" alt = "trash on beach">
+		</div>
+		<div class="w3-col w3-container w3-half">
+			<p>The ocean covers almost three quarters of our planet. Populations in coastal regions are growing and placing increasing pressure on coastal and marine ecosystems. Marine pollution of many kinds threatens the health of the ocean and its living resources. While the past decades have seen efforts at the local, national, and international levels to address the problems of marine pollution, more needs to be done.</p>		
+		</div> -->
 	</div>
 
 	<div class="w3-row">
-		<div class="w3-col w3-container m8 l9">
+		<div class="w3-col w3-container">
 			<h2>The Importance of Our Oceans</h2>
 			<p>Covering more than 70 percent of our planet, oceans are among the earth’s most valuable natural resources. They govern the weather, clean the air, help feed the world, and provide a living for millions. They also are home to most of the life on earth, from microscopic algae to the blue whale, the largest animal on the planet. Yet we’re bombarding them with pollution. By their very nature—with all streams flowing to rivers, all rivers leading to the sea—the oceans are the end point for so much of the pollution we produce on land, however far from the coasts we may be. And from dangerous carbon emissions to choking plastic to leaking oil to constant noise, the types of ocean pollution humans generate are vast. As a result, collectively, our impact on the seas is degrading their health at an alarming rate.</p>
 		</div>
-		<div class="w3-col w3-container m4 l3 w3-yellow">
-			<p>This part will occupy 12 columns on a small screen, 4 on a medium screen, and 3 on a large screen.</p>
-		</div>
 	</div>
+</div>
 </div>
 
 <!-- </body>
--- pollution.php
+++ pollution.php
@@ -1,4 +1,43 @@
-<?php include "inc/html-top.inc"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include "inc/html-top.inc"; ?>
+	<?php include "inc/css.inc"; ?>
+	<title></title>
+</head>
+<body>
+
+
+	<div class="content">
+	<div class="w3-container">
+
+		<div class="w3-row">
+			<h2>Types of Pollution</h2>
+			<div class="w3-col w3-container">
+				<img src = "images/numbers.png" alt = "pollution statistics">
+			</div>
+			<!-- <div class="w3-col w3-container m4 l3">
+				<p>The ocean covers almost three quarters of our planet. Populations in coastal regions are growing and placing increasing pressure on coastal and marine ecosystems. Marine pollution of many kinds threatens the health of the ocean and its living resources. While the past decades have seen efforts at the local, national, and international levels to address the problems of marine pollution, more needs to be done.</p>		
+			</div> -->
+		</div>
+
+		<div class="w3-row">
+			<div class="w3-col w3-container">
+				<h3>Oil</h3>
+				<p>Oil spills cause huge damage to the marine environment - but in fact are responsible for only around 12% of the oil entering the seas each year. According to a study by the US National Research Council, 36% comes down drains and rivers as waste and runoff from cities and industry.</p>
+			</div>
+			<!-- <div class="w3-col w3-container m4 l3">
+				<p>The ocean covers almost three quarters of our planet. Populations in coastal regions are growing and placing increasing pressure on coastal and marine ecosystems. Marine pollution of many kinds threatens the health of the ocean and its living resources. While the past decades have seen efforts at the local, national, and international levels to address the problems of marine pollution, more needs to be done.</p>		
+			</div> -->
+		</div>
+
+	<div class="w3-row">
+		<div class="w3-col w3-container">
+			<h2>The Importance of Our Oceans</h2>
+			<p>Covering more than 70 percent of our planet, oceans are among the earth’s most valuable natural resources. They govern the weather, clean the air, help feed the world, and provide a living for millions. They also are home to most of the life on earth, from microscopic algae to the blue whale, the largest animal on the planet. Yet we’re bombarding them with pollution. By their very nature—with all streams flowing to rivers, all rivers leading to the sea—the oceans are the end point for so much of the pollution we produce on land, however far from the coasts we may be. And from dangerous carbon emissions to choking plastic to leaking oil to constant noise, the types of ocean pollution humans generate are vast. As a result, collectively, our impact on the seas is degrading their health at an alarming rate.</p>
+		</div>
+	</div>
+</div>
 
 	<h2>Types of Pollution</h2>
     <img src = "images/numbers.png" alt = "pollution statistics">
