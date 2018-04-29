0b3e2edb7327d698c3cb4e86756043580c383a41
Conley <cernst3@u.rochester.edu>
2018-04-29 13:31:57 -0400

    something is wrong with the style sheets

--- impact.php
+++ impact.php
@@ -16,33 +16,39 @@
 
 			<div class="w3-row">
 				<h2>Global Impact</h2>
-				<div class="w3-col w3-container m4 l3">
-					<h3>How Much Trash Enters the Ocean?</h3>
-					<p>Approximately 1.4 billion pounds of trash per year enters the ocean. Over 80% of marine pollution comes from land-based activities.</p>	
-					<img src = "images/plasticfish.jpg" alt = "fish made o' plastic">	
-				</div>
 				<div class="w3-col w3-container m8 l9">
 					<!-- <img src = "images/beachedWhale.jpg" alt = "beached, dead whale"> -->
 					<img src = "images/trashbag.jpg" alt = "trashbag floating in ocean">
 				</div>
+				<div class="w3-col w3-container m4 l3">
+					<h3>How Much Trash Enters the Ocean?</h3>
+					<p>Approximately 1.4 billion pounds of trash per year enters the ocean. Over 80% of marine pollution comes from land-based activities.</p>	
+					<!-- <img src = "images/plasticfish.jpg" alt = "fish made o' plastic">	 -->
+				</div>
+				<!-- <div class="w3-col w3-container m8 l9">
+					<img src = "images/beachedWhale.jpg" alt = "beached, dead whale">
+					<img src = "images/trashbag.jpg" alt = "trashbag floating in ocean">
+				</div> -->
 			</div>
 
 			<div class="w3-row">
-				<div class="w3-col w3-container w3-half">
+				<!-- <div class="w3-col w3-container w3-half">
 					<img src = "images/beachedWhale.jpg" alt = "beached, dead whale"> 
-				</div>
+				</div> -->
 				<div class="w3-col w3-container w3-half">
 					<h3>Impact on Marine Life</h3>
 					<p>Plastic garbage, which decomposes very slowly, is often mistaken for food by marine animals. High concentrations of plastic material, particularly plastic bags, have been found blocking the breathing passages and stomachs of many marine species, including whales, dolphins, seals, puffins, and turtles. Plastic six-pack rings for drink bottles can also choke marine animals.</p>
 
 					<h3>Impact on Seafood</h3>
 					<p>Pollution can have an adverse affect on the food that we eat. Heavy metals and other contaminants can accumulate in seafood and make it harmful to eat. More than one-third of the shellfish-growing waters of the United States are adversely affected by coastal pollution.</p>
-
+				</div>
+				<div class="w3-col w3-container w3-half">
+					<img src = "images/beachedWhale.jpg" alt = "beached, dead whale"> 
 				</div>
 			</div>
 			<div class="w3-row">
 				<div class="w3-col w3-container w3-half">
-					<!-- <img src = "images/beachedWhale.jpg" alt = "beached, dead whale">  -->
+					<img src = "images/trash.jpg" alt = "soda holder floating"> 
 				</div>
 				<div class="w3-col w3-container w3-half">
 					<h3>Prevention</h3>
