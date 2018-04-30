3a09295adf899c641e27de4fba73c2f3ffb9b0ac
Conley <cernst3@u.rochester.edu>
2018-04-30 11:52:09 -0400

    next pg cols

--- impact.php
+++ impact.php
@@ -10,6 +10,50 @@
 	<title></title>
 </head>
 <body>
+	
+	<div class="content">
+	<div class="row">
+		<h1 style="text-align: center;">Global Impact</h1>
+	</div>
+	<div class="row">
+		<div class="column">
+			<img src = "images/trashbag.jpg" alt = "trashbag floating in ocean">
+		</div>
+		<div class="column">
+			<h3>How Much Trash Enters the Ocean?</h3>
+			<p>Approximately 1.4 billion pounds of trash per year enters the ocean. Over 80% of marine pollution comes from land-based activities.</p>
+
+			<h3>Prevention</h3>
+					<ul>
+						<li>Reducing, reusing, and recycling</li>
+						<li>Not littering</li>
+						<li>Carrying a reusable shopping bag instead of plastic bags</li>
+						<li>Cut apart each plastic ring of a six-pack holder to prevent marine life and other wildlife from becoming trapped and strangled.</li>
+						<li>Volunteer to clean-up an area of beach if you live nearby.</li>
+					</ul>
+		</div>
+	</div>
+	
+
+	<div class="row">
+	  		<div class="column-twothird">
+	    		<h3>Impact on Marine Life</h3>
+				<p>Plastic garbage, which decomposes very slowly, is often mistaken for food by marine animals. High concentrations of plastic material, particularly plastic bags, have been found blocking the breathing passages and stomachs of many marine species, including whales, dolphins, seals, puffins, and turtles. Plastic six-pack rings for drink bottles can also choke marine animals.</p>
+	  		</div>
+	  		<div class="column-third">
+	    		<img src = "images/beachedWhale.jpg" alt = "beached, dead whale"> 
+	    	</div>
+	</div>
+	<div class="row">
+	  		<div class="column-twothird">
+	    		<h3>Impact on Seafood</h3>
+				<p>Pollution can have an adverse affect on the food that we eat. Heavy metals and other contaminants can accumulate in seafood and make it harmful to eat. More than one-third of the shellfish-growing waters of the United States are adversely affected by coastal pollution.</p>
+	  		</div>
+	  		<div class="column-third">
+	    		<img src = "images/trash.jpg" alt = "soda holder floating"> 
+	    	</div>
+	</div>
+</div>
 
 	<div class="content">
 		<div class="w3-container">
