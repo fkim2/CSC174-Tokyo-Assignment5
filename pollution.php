6b598a4c04120d7618600fe2bbd66934544ca8f4
Conley <cernst3@u.rochester.edu>
2018-04-30 11:56:25 -0400

    fixed pollution page

--- pollution.php
+++ pollution.php
@@ -9,64 +9,42 @@
 
 
 	<div class="content">
-		<div class="w3-container">
-
-			<div class="w3-row">
-				<h2>Types of Pollution</h2>
-				<!-- <div class="w3-col w3-container w3-twothird">
-					<img src = "images/numbers.png" alt = "pollution statistics">
-				</div> -->
-				<div class="w3-col w3-container w3-half">
-					<h3>Oil</h3>
+		<div class="row">
+			<h1 style="text-align: center;">Types of Pollution</h1>
+		</div>
+		<div class="row">
+			<div class="column">
+				<h3>Oil</h3>
 					<p>Oil spills cause huge damage to the marine environment - but in fact are responsible for only around 12% of the oil entering the seas each year. According to a study by the US National Research Council, 36% comes down drains and rivers as waste and runoff from cities and industry.</p>
 
 					<h3>Fertilizers</h3>
 					<p>Fertilizer runoff from farms and lawns is a huge problem for coastal areas. The extra nutrients cause eutrophication - flourishing of algal blooms that deplete the water's dissolved oxygen and suffocate other marine life.</p>
-
-					<!-- <h3>Garbage</h3>
-					<p>Solid garbage also makes its way to the ocean. Plastic bags, balloons, glass bottles, shoes, packaging material – if not disposed of correctly, almost everything we throw away can reach the sea.</p> -->
-<!-- 
-					<h3>Sewage disposal</h3>
-					<p>In many parts of the world, sewage flows untreated, or under-treated, into the ocean. For example, 80% of urban sewage discharged into the Mediterranean Sea is untreated.</p>
-
-					<h3>Toxic chemicals</h3>
-					<p>Almost every marine organism, from the tiniest plankton to whales and polar bears, is contaminated with man-made chemicals, such as pesticides and chemicals used in common consumer products.Some of these chemicals enter the sea through deliberate dumping. For centuries, the oceans have been a convenient dumping ground for waste generated on land. This continued until the 1970s, with dumping at sea the accepted practise for disposal of nearly everything, including toxic material such as pesticides, chemical weapons, and radioactive waste.</p> -->
-				</div>
-				<div class="w3-col w3-container w3-half">
-					<div class="image-holder">
-						<img src = "images/numbers.png" alt = "pollution statistics">
-					</div>
-					<!-- <div class="image-holder">
-						<img src = "images/filler.png" alt = "pollution graphic">
-					</div> -->
-				</div>
-				
 			</div>
+			<div class="column">
+				<img src = "images/numbers.png" alt = "pollution statistics">
+			</div>
+		</div>
 
-			<div class="w3-row">
-				<div class="w3-col w3-container w3-half">
+		<div class="row">
+			<div class="column">
+					<img src = "images/filler.png" alt = "pollution graphic">
+			</div>
+			<div class="column">
 					<h3>Garbage</h3>
 					<p>Solid garbage also makes its way to the ocean. Plastic bags, balloons, glass bottles, shoes, packaging material – if not disposed of correctly, almost everything we throw away can reach the sea.</p>
 
 					<h3>Sewage disposal</h3>
 					<p>In many parts of the world, sewage flows untreated, or under-treated, into the ocean. For example, 80% of urban sewage discharged into the Mediterranean Sea is untreated.</p>
-
-					<!-- <h3>Toxic chemicals</h3>
-					<p>Almost every marine organism, from the tiniest plankton to whales and polar bears, is contaminated with man-made chemicals, such as pesticides and chemicals used in common consumer products.Some of these chemicals enter the sea through deliberate dumping. For centuries, the oceans have been a convenient dumping ground for waste generated on land. This continued until the 1970s, with dumping at sea the accepted practise for disposal of nearly everything, including toxic material such as pesticides, chemical weapons, and radioactive waste.</p> -->
-				</div>
-				<div class="w3-col w3-container w3-half">
-						<img src = "images/filler.png" alt = "pollution graphic">
-				</div>
 			</div>
+		</div>
 
-			<div class="w3-row">
-				<div class="w3-col w3-container w3-half">
-					<h3>Toxic chemicals</h3>
+		<div class="row">
+			<div class="column">
+				<h3>Toxic chemicals</h3>
 					<p>Almost every marine organism, from the tiniest plankton to whales and polar bears, is contaminated with man-made chemicals, such as pesticides and chemicals used in common consumer products.Some of these chemicals enter the sea through deliberate dumping. For centuries, the oceans have been a convenient dumping ground for waste generated on land. This continued until the 1970s, with dumping at sea the accepted practise for disposal of nearly everything, including toxic material such as pesticides, chemical weapons, and radioactive waste.</p>
-				</div>
-				<div class="w3-col w3-container w3-half">
-						<img src = "images/deadfish.jpg" alt = "another grpahic">
-				</div>
+			</div>
+			<div class="column">
+				<img src = "images/deadfish.jpg" alt = "another grpahic">
 			</div>
 		</div>
 	</div>
